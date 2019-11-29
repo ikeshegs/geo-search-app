f310035055c9525988023f1147d9fa9a7fd661b4
Okoro Ikechukwu <ikeshegs@gmail.com>
2019-11-29 22:48:54 +0100

    modify code

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -16,21 +16,15 @@ document.getElementById('search-btn').addEventListener('click', e => {
     window.alert('Please Input a valid Location');
     return false;
   }
-  console.log(document.getElementById('search').value);
-  // location.assign(`/?${document.getElementById('search').value}`);
+  searchInput();
+  getWeather();
 });
 
 document.getElementById('search-btn').addEventListener('click', e => {
   e.preventDefault();
 
   if (location.search) {
-    searchInput();
-    getWeather();
+    // searchInput();
+    // getWeather();
   }
-  console.log(location.search.replace('?', ''));
 });
-
-console.log(location.href)
-
-// searchForm.addEventListener('submit', searchInput);
-// searchForm.addEventListener('submit', getWeather);
